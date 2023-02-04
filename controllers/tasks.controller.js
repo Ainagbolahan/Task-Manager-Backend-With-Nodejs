922cc10c78b6f6a86504e6b00f732ddbc974a3bc
Aina Gbolahan <ainagbolahan100@gmail.com>
2023-02-04 21:39:27 +0100

    added controllers

--- controllers/tasks.controller.js
+++ controllers/tasks.controller.js
@@ -1,26 +1,114 @@
-const getAllTasks = async (req, res) => {
-    res.send('Get All Task')
-};
+const { Tasks } = require('../models/task.models')
 
+const getAllTasks = async (req, res) => {
+	try {
+		const allTask = Tasks.find({})
+
+		return res.status(200).json({
+			meassage: 'task fetched Sucessfully',
+			allTask,
+		})
+	} catch (err) {
+		console.log(err)
+		return res.status(500).json({
+			message: 'Unable to create task',
+		})
+	}
+}
 
 const createTasks = async (req, res) => {
-    res.send('ACreate task')
-};
+	try {
+		const task = await Tasks.create(req.body)
+		return res.status(201).json({
+			meassage: 'task Created Sucessfully',
+			task,
+		})
+	} catch (err) {
+		console.log(err)
+		return res.status(500).json({
+			message: 'Unable to create task',
+		})
+	}
+}
 
 const getTasks = async (req, res) => {
-    res.send('Gget single task')
-};
+	try {
+		const taskId = req.params.id
+		const getSingleTask = Tasks.findOne({ _id: taskId })
+
+		if (!getSingleTask) {
+			return res.status(401).json({
+				message: 'Unable to get id',
+			})
+		}
+
+		return res.status(200).json({
+			meassage: 'task fetched Sucessfully',
+			getSingleTask,
+		})
+	} catch (err) {
+		console.log(err)
+		return res.status(500).json({
+			message: 'Unable to create task',
+		})
+	}
+}
+
 const updateTasks = async (req, res) => {
-    res.send('update task')
-};
+	try {
+		const taskId = req.params.id
+		const { body: taskBody } = req.body
+		// const update ={body:taskBody.name}
+		const updateSingleTask = await Tasks.findOneAndUpdate({ _id: taskId }, taskBody, {
+			new: true,
+			runValidators: true,
+		})
+
+		if (!updateSingleTask) {
+			return res.status(401).json({
+				message: 'Unable to get id',
+			})
+		}
+
+		return res.status(200).json({
+			meassage: 'task Deleted Sucessfully',
+			updateSingleTask,
+		})
+	} catch (err) {
+		console.log(err)
+		return res.status(500).json({
+			message: 'Unable to create task',
+		})
+	}
+}
+
 const deleteTasks = async (req, res) => {
-    res.send('delete task')
-};
+	try {
+		const taskId = req.params.id
+		const deleteSingleTask = Tasks.findOneAndDelete({ _id: taskId })
+
+		if (!deleteSingleTask) {
+			return res.status(401).json({
+				message: 'Unable to get id',
+			})
+		}
+
+		return res.status(200).json({
+			meassage: 'task Deleted Sucessfully',
+			deleteSingleTask,
+		})
+	} catch (err) {
+		console.log(err)
+		return res.status(500).json({
+			message: 'Unable to create task',
+		})
+	}
+}
 
 module.exports = {
-    getAllTasks,
-    createTasks,
-    getTasks,
-    updateTasks,
-    deleteTasks
+	getAllTasks,
+	createTasks,
+	getTasks,
+	updateTasks,
+	deleteTasks,
 }
--- models/task.models.js
+++ models/task.models.js
@@ -1,6 +1,17 @@
-const { Schema, model } = require("mongoose");
+const { Schema, model } = require('mongoose')
 
 const taskSchema = new Schema({
-    name: String,
-    completed: Boolean
+	name: {
+		type: String,
+		trim: true,
+		required: [true, 'please provide name'],
+	},
+	completed: {
+		type: Boolean,
+		default: false,
+	},
 })
+
+const Tasks = model('Task', taskSchema)
+
+module.exports = { Tasks }
